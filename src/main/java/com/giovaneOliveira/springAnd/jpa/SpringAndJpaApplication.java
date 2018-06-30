10ec45c127f064c1a47a32b07f1236ad11731c26
Giovane <giovaneoliveira_ti@hotmail.com>
2018-06-30 10:57:21 -0300

    Projeto Criado

---
+++ src/main/java/com/giovaneOliveira/springAnd/jpa/SpringAndJpaApplication.java
@@ -0,0 +1,12 @@
+package com.giovaneOliveira.springAnd.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndJpaApplication.class, args);
+	}
+}
